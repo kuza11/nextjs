a7e0e7920dde08cb57beed8bdfe4cf0bd8679933
zvolaja <zvolaja@student.uzlabina.cz>
2023-01-26 09:14:52 +0100

    new api

--- src/pages/api/languages/index.exp.ts
+++ src/pages/api/languages/index.exp.ts
@@ -1,7 +1,66 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { insertDB, readDB } from "../../../../DB_functions/DB_lang_funcs";
+import { createRouter, expressWrapper } from "next-connect";
+import cors from "cors";
 
-export default async function RWlanguages(
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router
+  .use(expressWrapper(cors())) // express middleware are supported if you wrap it with expressWrapper
+  .use(async (req, res, next) => {
+    const start = Date.now();
+    await next(); // call next in chain
+    const end = Date.now();
+    console.log(`Request took ${end - start}ms`);
+  })
+  .get(async (req, res) => {
+    try{
+      const data = await readDB({table: "languages"});
+      if(data) res.status(200).json(data);
+      else res.status(500).json({message: "error"});
+    }
+    catch (error){
+      res.status(400).json({error: error, message: "Probably wrong data in body"});
+    }
+  })
+  .post(async (req, res) => {
+    try{
+      const data = await insertDB({table: "languages"}, {name: req.body.name});
+      if(data) res.status(201).json(data);
+      else res.status(500).json({message: "error"});
+    }
+    catch (error){
+      res.status(400).json({error: error, message: "Probably wrong data in body"});
+    }
+  })/*
+  .put(
+    async (req, res, next) => {
+      // You may want to pass in NextApiRequest & { isLoggedIn: true }
+      // in createRouter generics to define this extra property
+      if (!req.isLoggedIn) throw new Error("thrown stuff will be caught");
+      // go to the next in chain
+      return next();
+    },
+    async (req, res) => {
+      const user = await updateUser(req.body.user);
+      res.json({ user });
+    }
+  );*/
+
+// create a handler from router with custom
+// onError and onNoMatch
+export default router.handler({
+  onError: (err, req, res) => {
+    console.error(err);
+    res.status(500).end("Something broke!");
+  },
+  onNoMatch: (req, res) => {
+    res.status(405).json({ message: "Method Not Allowed" });
+  },
+});
+
+
+/*export default async function RWlanguages(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -21,4 +80,4 @@ export default async function RWlanguages(
     res.status(400).json({error: error, message: "Probably wrong data in body"});
   }
   
-}
+}*/
